8d7a705bda52bcf2b3aeccb116878f6424e129e4
Vishal Naidu <naiduvishal13@gmail.com>
2020-01-11 20:42:21 -0700

    Created pickup prefab

--- Assets/Scripts/Gameplay/Pickup.cs
+++ Assets/Scripts/Gameplay/Pickup.cs
@@ -5,17 +5,34 @@ using UnityEngine;
 public class Pickup : MonoBehaviour
 {
 	private CircleCollider2D _collider;
+	private bool _isInactive = false;
+	private float _inactivityTime = 0.8f;
+	private float _timer = 0f;
 
     void Start()
     {
 		_collider = GetComponent<CircleCollider2D>();
     }
 
+	void Update()
+	{
+		// Scaling
+		if (_isInactive)
+		{
+			transform.localScale = Vector3.Lerp(transform.localScale, Vector3.zero, Time.deltaTime * 8f);
+			_timer += Time.deltaTime;
+			if (_timer > _inactivityTime)
+				Destroy(gameObject);
+		}
+	}
+
 	private void OnCollisionEnter2D(Collision2D collision)
 	{
+		// Collision with player
 		if (collision.gameObject.CompareTag("Player"))
 		{
 			Player.instance.OnPickupHit();
+			_isInactive = true;
 		}
 	}
 }
