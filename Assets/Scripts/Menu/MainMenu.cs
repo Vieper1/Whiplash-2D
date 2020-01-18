6b44c4e1d4cc7602fb5b78432749abd6a1b0c005
Davian One <naiduvishal13@gmail.com>
2020-01-17 23:26:20 -0700

    Added HighScore text

--- Assets/Scripts/Menu/MainMenu.cs
+++ Assets/Scripts/Menu/MainMenu.cs
@@ -5,9 +5,17 @@ using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour
 {
+	public GameObject HighScoreText;
+
+	int highestScore = 0;
+
     void Start()
     {
-        
+		highestScore = SaveGame.LoadScore();
+		if (highestScore == 0)
+		{
+			HighScoreText.SetActive(false);
+		}
     }
 
     void Update()
